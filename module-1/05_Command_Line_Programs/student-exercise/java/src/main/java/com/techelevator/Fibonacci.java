3ef40c75db776de920d4666e56f6e2bde3bd5002
dave kruse <dave_kruse@att.net>
2020-09-19 13:40:09 -0400

    Exercise #5.  Fibonacci, Temp, and Length modules completed.

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,42 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		
+		// Please enter the Fibonacci number: 25
+		System.out.println("Please enter the Fibonacci number: ");
+		int num = myScanner.nextInt();
+		myScanner.nextLine();	// Eat newline character at end of previous input line.
+		//System.out.println("Fib num: " + num);
 
+		int prevNum = 0;
+		int curNum = 1;
+		int fibNum = 1;
+		
+		if (num >= 1) {
+			System.out.print(prevNum);
+		}
+		if (num >= 2) {
+			System.out.print(", ");
+			System.out.print(curNum);
+		}
+		
+		while (fibNum <= num) {
+			//if (x != 0) {
+				System.out.print(", ");
+			//}
+			System.out.print(fibNum);
+			
+			prevNum = curNum;
+			curNum = fibNum;
+			fibNum = prevNum + curNum;
+		}
+		
+		myScanner.close();
 	}
 
 }
