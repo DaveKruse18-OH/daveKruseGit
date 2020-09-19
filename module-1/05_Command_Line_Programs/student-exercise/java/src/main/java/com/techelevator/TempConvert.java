cf632831682455ca1766b2a36d2f3c667dd267a3
dave kruse <dave_kruse@att.net>
2020-09-19 10:05:49 -0400

    Exercise 5, TempConversion done.

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,30 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
-
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Please enter the temperature: ");
+		float temp = myScanner.nextFloat();
+		myScanner.nextLine();	// Eat newline character at end of number line.
+		System.out.println("Is the temperature in (C)elsius, or (F)ahrenheit?");
+		String cOrF = myScanner.nextLine().toUpperCase();
+		System.out.println("temp: " + temp);
+		System.out.println("cOrF: " + cOrF);
+		
+		float newTemp = 0;
+		if (cOrF.equals("C")) {
+			newTemp = temp * 1.8F + 32F;
+			System.out.println(temp + "C" + " is " + newTemp + "F");
+		} else {
+			newTemp = (temp - 32F) / 1.8F;
+			System.out.println(temp + "F" + " is " + newTemp + "C");
+		}
+		
+		myScanner.close();
 	}
 
 }
