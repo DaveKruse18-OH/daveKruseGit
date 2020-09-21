56ae021b4bea9153967866310cf7cb2b4bb89e1b
dave kruse <dave_kruse@att.net>
2020-09-21 08:55:11 -0400

    Completed exercise #5.

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -13,44 +13,55 @@ public class DecimalToBinary {
 		
 		String answer = null;
 		for (int x = 0; x < nums.length; x++) {
-			int divisor = 2;
+			int divisor = 1;
 			System.out.println("nums: " + nums[x]);
 			int intNum = Integer.parseInt(nums[x]);
 			
-			while (intNum > 0) {
-				int rem = 0;
-				rem = intNum % divisor;
-				
-				// Special case to get started (divisor = 2, num = 0, 1, 2, or 3).
-				if (divisor == 2) {
-					if (rem == 0) {
-						answer = "10";
-						intNum -= divisor;
+			if (intNum > 0) {
+				// Start - find starting divisor (largest).
+				boolean divFound = false;
+				int whole = 0;
+				while (!divFound) {
+					whole = intNum / divisor;
+					if (whole == 1) {
+						divFound = true;
+					} else {
 						divisor = divisor * 2;
-					} else { // Odd number.
-						int temp = (intNum - 1) / divisor;
-						if (temp == 1) {
-							answer = "11";
-							intNum -= 3;
-							divisor = divisor * 2;
-						} else {
-							answer = "1";
-							intNum -= 1;
-							divisor = divisor * 2;
-						}
 					}
 				}
-				// End special case.
+				System.out.println("Starting divisor: " + divisor);
+				// End - find starting divisor.
 				
-				
-				//if (rem == 0) {
-				//	answer = "1" + answer;
-				//	intNum -= divisor;
-				//	divisor = divisor * 2;
-				//}
-			}
-			
-			System.out.println("divisor: " + divisor);
+				// Now find the answer, from the starting divisor.
+				boolean answerFound = false;
+				while (!answerFound) {
+					if (divisor == 1) {
+						answerFound = true;
+					}
+					//int rem = 0;
+					whole = 0;
+					//rem = intNum % divisor;
+					whole = intNum / divisor;
+					if (whole == 1) {
+						if (answer == null) {
+							answer = "1";
+						} else {
+							answer = answer + "1";
+						}
+						intNum = intNum - divisor;
+						divisor = divisor / 2;
+					} else {
+						if (answer == null) {
+							answer = "0";
+						} else {
+							answer = answer + "0";
+						}
+						//intNum = intNum - divisor;
+						divisor = divisor / 2;
+					}
+					
+				}
+			} // End if (intNum > 0).
 		}
 
 		System.out.println("answer: " + answer);
