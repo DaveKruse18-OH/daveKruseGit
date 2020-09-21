a4edf85cc62704ce44bbd1d53ac861822e433ab6
dave kruse <dave_kruse@att.net>
2020-09-21 15:55:07 -0400

    Exercise #5 complete.

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -14,35 +14,33 @@ public class DecimalToBinary {
 		String answer = null;
 		for (int x = 0; x < nums.length; x++) {
 			int divisor = 1;
-			System.out.println("nums: " + nums[x]);
+			//System.out.println("nums: " + nums[x]);
 			int intNum = Integer.parseInt(nums[x]);
 			
-			if (intNum > 0) {
+			if (intNum > 0) {	// Don't calculate binary code for 0 or negative numbers.
 				// Start - find starting divisor (largest).
 				boolean divFound = false;
-				int whole = 0;
+				int quotient = 0;
 				while (!divFound) {
-					whole = intNum / divisor;
-					if (whole == 1) {
+					quotient = intNum / divisor;
+					if (quotient == 1) {
 						divFound = true;
 					} else {
 						divisor = divisor * 2;
 					}
 				}
-				System.out.println("Starting divisor: " + divisor);
+				//System.out.println("Starting divisor: " + divisor);
 				// End - find starting divisor.
 				
-				// Now find the answer, from the starting divisor.
+				// Now find the answer, from the starting (largest) divisor.
 				boolean answerFound = false;
 				while (!answerFound) {
 					if (divisor == 1) {
 						answerFound = true;
 					}
-					//int rem = 0;
-					whole = 0;
-					//rem = intNum % divisor;
-					whole = intNum / divisor;
-					if (whole == 1) {
+					quotient = 0;
+					quotient = intNum / divisor;
+					if (quotient == 1) {
 						if (answer == null) {
 							answer = "1";
 						} else {
@@ -62,9 +60,13 @@ public class DecimalToBinary {
 					
 				}
 			} // End if (intNum > 0).
+			if (answer == null) {
+				answer = "0";
+			}
+			//460 in binary is 111001100
+			System.out.println(nums[x] + " in binary is " + answer);
+			answer = null;
 		}
-
-		System.out.println("answer: " + answer);
 	}
 
 }
