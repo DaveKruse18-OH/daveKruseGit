0a6f0c2d05b7aae53b8eed369d0d1759477cf278
dave kruse <dave_kruse@att.net>
2020-09-24 17:33:06 -0400

    Exercise #9 partially complete.

--- module-1/09_Introduction_Classes/student-exercise/java/src/main/java/com/techelevator/company/Company.java
+++ module-1/09_Introduction_Classes/student-exercise/java/src/main/java/com/techelevator/company/Company.java
@@ -1,9 +1,52 @@
 package com.techelevator.company;
 
 public class Company {
-
-	//
-	// Write code here
-	//
+	private String name;
+	private int numberOfEmployees;
+	private double revenue;
+	private double expenses;
 	
+	// Constructor.
+	public Company() {
+		// Do nothing.
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getCompanySize() {
+		String ret = null;
+		if (numberOfEmployees <= 50) {
+			ret  = "small";
+		} else if ((numberOfEmployees >= 51) && (numberOfEmployees <= 250)) {
+			ret = "medium";
+		} else {
+			ret = "large";
+		}
+		return ret;
+	}
+	public double getProfit() {
+		return revenue - expenses;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	public double getRevenue() {
+		return revenue;
+	}
+	public void setRevenue(double revenue) {
+		this.revenue = revenue;
+	}
+	public double getExpenses() {
+		return expenses;
+	}
+	public void setExpenses(double expenses) {
+		this.expenses = expenses;
+	}
 }
