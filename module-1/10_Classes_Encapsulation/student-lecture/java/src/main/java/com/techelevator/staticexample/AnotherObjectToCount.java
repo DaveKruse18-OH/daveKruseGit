fcb6d0dc0ab90ed14223483c37e0c1682b2165b6
Steve Carmichael <steve@techelevator.com>
2020-09-25 09:20:12 -0400

    added static example

---
+++ module-1/10_Classes_Encapsulation/student-lecture/java/src/main/java/com/techelevator/staticexample/AnotherObjectToCount.java
@@ -0,0 +1,18 @@
+package com.techelevator.staticexample;
+
+public class AnotherObjectToCount {
+
+	   //this is an example of an object counter that does not have a static variable
+	   public int count = 0;
+	     
+	   public void increment()
+	   {
+	       this.count++;
+	   }	
+	   
+	   public int getCount() {
+		   return count;
+	   }
+	
+	
+}
