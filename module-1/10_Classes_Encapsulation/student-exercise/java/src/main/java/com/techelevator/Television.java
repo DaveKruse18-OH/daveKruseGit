ae9079e09cb1c814caebb464773c86d1d7a5d6b4
dave kruse <dave_kruse@att.net>
2020-09-26 17:01:13 -0400

    Exercise #10 partially complete.

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,77 @@
 package com.techelevator;
 
 public class Television {
-
+	private boolean isOn = false;
+	private int currentChannel = 3;
+	private int currentVolume = 2;
+	
+	public Television() {
+		// Do nothing.
+	}
+	
+	public void turnOff() {
+		isOn = false;
+	}
+	
+	public void turnOn() {
+		isOn = true;
+		currentChannel = 3;
+		currentVolume = 2;
+	}
+	
+	public void changeChannel(int newChannel) {
+		if ((isOn) && (newChannel >= 3) && (newChannel <= 18)) {
+			currentChannel = newChannel;
+		}
+	}
+	
+	public void channelUp() {
+		if (isOn) {
+			currentChannel++;
+			if (currentChannel > 18) {
+				currentChannel = 3;
+			}
+		}
+	}
+	
+	public void channelDown() {
+		if (isOn) {
+			currentChannel--;
+			if (currentChannel < 3) {
+				currentChannel = 18;
+			}
+		}
+	}
+	
+	public void raiseVolume() {
+		if (isOn) {
+			currentVolume++;
+			if (currentVolume > 10) {
+				currentVolume = 10;
+			}
+		}
+	}
+	
+	public void lowerVolume() {
+		if (isOn) {
+			currentVolume--;
+			if (currentVolume < 0) {
+				currentVolume = 0;
+			}
+		}
+	}
+	
+	public boolean isOn() {
+		return isOn;
+	}
+	
+	public int getCurrentChannel() {
+		return currentChannel;
+	}
+	
+	public int getCurrentVolume() {
+		return currentVolume;
+	}
+	
+	
 }
