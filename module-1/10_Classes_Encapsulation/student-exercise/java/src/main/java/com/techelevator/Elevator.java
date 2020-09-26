3fcd6b91d980aff5e62142c0a994ae7410dd18dc
dave kruse <dave_kruse@att.net>
2020-09-26 17:14:51 -0400

    Exercise #10 complete.

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,54 @@
 package com.techelevator;
 
 public class Elevator {
+	private int currentFloor = 0;
+	private int numberOfFloors = 0;
+	private boolean doorOpen = false;
 
+	public Elevator(int totalNumberOfFloors) {
+		numberOfFloors = totalNumberOfFloors;
+		currentFloor = 1;
+	}
+	
+	public void openDoor() {
+		doorOpen = true;
+	}
+	
+	public void closeDoor() {
+		doorOpen = false;
+	}
+	
+	public void goUp(int desiredFloor) {
+		if ((!doorOpen) && (desiredFloor > currentFloor)) {
+			if (desiredFloor > numberOfFloors) {
+				currentFloor = numberOfFloors;
+			} else {
+				currentFloor = desiredFloor;
+			}
+		}
+	}
+	
+	public void goDown(int desiredFloor) {
+		if ((!doorOpen) && (desiredFloor < currentFloor)) {
+			if (desiredFloor < 1) {
+				currentFloor = 1;
+			} else {
+				currentFloor = desiredFloor;
+			}
+		}
+	}
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+	
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+	
+	
 }
