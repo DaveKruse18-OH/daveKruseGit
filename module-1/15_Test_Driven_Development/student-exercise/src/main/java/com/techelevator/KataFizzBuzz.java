9a035dcc1793fe9f7e75e03ae6f4253eb40ec7f4
dave kruse <dave_kruse@att.net>
2020-10-04 15:33:30 -0400

    Added initial test case to verify we can call the FizzBuzz method OK.

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	
+	public String fizzBuss(int num) {
+		return null;
+	}
 
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	private KataFizzBuzz fB;
+	
+	@Before
+	public void beforeAllTests() {
+		fB = new KataFizzBuzz();
+	}
+	
+	@Test
+	public void testDivBy3() {
+		String ret1 = fB.fizzBuss(3);
+	}
 
 }
