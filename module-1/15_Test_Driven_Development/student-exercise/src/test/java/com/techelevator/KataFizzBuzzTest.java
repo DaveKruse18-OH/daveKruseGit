3c21e658b054e40f835c22fc724d5d0ce8250afb
dave kruse <dave_kruse@att.net>
2020-10-04 16:07:32 -0400

    Wrote test case to test number within range (1-100).  Works without any code changes.

--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -65,4 +65,21 @@ public class KataFizzBuzzTest {
 		Assert.assertEquals("FizzBuzz", ret4);
 		Assert.assertEquals("FizzBuzz", ret5);
 	}
+	
+	@Test
+	public void testWithinRange() {
+		String ret1 = fB.fizzBuss(1);
+		String ret2 = fB.fizzBuss(100);
+		String ret3 = fB.fizzBuss(3);
+		String ret4 = fB.fizzBuss(10);
+		String ret5 = fB.fizzBuss(15);
+		String ret6 = fB.fizzBuss(30);
+		
+		Assert.assertEquals("1", ret1);
+		Assert.assertEquals("Buzz", ret2);
+		Assert.assertEquals("Fizz", ret3);
+		Assert.assertEquals("Buzz", ret4);
+		Assert.assertEquals("FizzBuzz", ret5);
+		Assert.assertEquals("FizzBuzz", ret6);
+	}
 }
