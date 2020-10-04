5055e2938a20c543120bb590d61d51a03f549297
dave kruse <dave_kruse@att.net>
2020-10-04 15:58:45 -0400

    Wrote the FizzBuzz test case which fails.

--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -49,5 +49,20 @@ public class KataFizzBuzzTest {
 		Assert.assertEquals("Buzz", ret2);
 		Assert.assertEquals("Buzz", ret3);
 		Assert.assertEquals("Buzz", ret4);
-	}	
+	}
+	
+	@Test
+	public void testRetFizzBuzz() {
+		String ret1 = fB.fizzBuss(1);
+		String ret2 = fB.fizzBuss(3);
+		String ret3 = fB.fizzBuss(10);
+		String ret4 = fB.fizzBuss(15);
+		String ret5 = fB.fizzBuss(30);
+		
+		Assert.assertEquals("Buzz", ret1);
+		Assert.assertEquals("Buzz", ret2);
+		Assert.assertEquals("Buzz", ret3);
+		Assert.assertEquals("Buzz", ret4);
+		Assert.assertEquals("Fizz", ret5);
+	}
 }
