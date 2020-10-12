848f1690a9d25543f53bb5f2ea0cd4bf3d0ce554
dave kruse <dave_kruse@att.net>
2020-10-12 18:22:36 -0400

    In progress push.

--- module-2/01_Introduction_to_Databases/student-exercise/intro-to-databases-exercises.sql
+++ module-2/01_Introduction_to_Databases/student-exercise/intro-to-databases-exercises.sql
@@ -25,25 +25,52 @@ from country
 where continent = 'Asia';
 
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
-
+select name, countrycode, population
+from city
+where population > 8000000;
 
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
+select name, countrycode, population
+from city
+where population < 1000;
 
 -- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
+select name, continent, gnp
+from country
+where gnp > 1000000;
 
 -- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
+select name, continent, population, gnp, lifeexpectancy
+from country
+where lifeexpectancy > 80;
 
 -- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
+select name, population
+from city
+where countrycode = 'USA' and population > 1000000;
 
 -- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
+select name, population
+from city
+where countrycode = 'CHN' and population > 1000000;
 
 -- 11. The name and region of all countries in North or South America (51 rows)
+select name, region
+from country
+where continent = 'North America' or continent = 'South America';
 
 -- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
+select name, continent, headofstate
+from country
+where governmentform like '%Monarchy%';
 
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
+select name
+from city
+where countrycode = 'USA' and population > 1000000 and population < 2000000;
 
 -- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
+
 
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
 
