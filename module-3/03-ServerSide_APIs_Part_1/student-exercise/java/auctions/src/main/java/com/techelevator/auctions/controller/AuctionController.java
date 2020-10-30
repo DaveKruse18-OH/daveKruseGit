92da5eb16ceeaaa3b698dd5e241573894b9d8e58
dave kruse <dave_kruse@att.net>
2020-10-29 20:35:43 -0400

    Module #3 - Exercise #3 Complete.

--- module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -18,8 +18,16 @@ public class AuctionController {
     }
 
     @RequestMapping(path = "", method = RequestMethod.GET)
-    public List<Auction> getAllAuctions() {
-    	return dao.list();
+    public List<Auction> getAuctions(@RequestParam (defaultValue = "") String title_like, @RequestParam (defaultValue = "0") double currentBid_lte) {
+    	if (title_like.equals("") && currentBid_lte == 0) {
+    		return dao.list();
+    	} else if ((!title_like.equals("")) && (currentBid_lte == 0)) {
+    		return dao.searchByTitle(title_like);
+    	} else if ((title_like.contentEquals("")) && (currentBid_lte != 0)) {
+    		return dao.searchByPrice(currentBid_lte);
+    	} else {
+    		return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+    	}
     }
     
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
@@ -27,4 +35,13 @@ public class AuctionController {
     	return dao.get(id);
     }
     
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Auction addAuction(@RequestBody Auction auction) {
+    	return dao.create(auction);
+    }
+    
+    //@RequestMapping(path = "/search", method = RequestMethod.GET)
+    //public List<Auction> searchAuctionTitle(@RequestParam (defaultValue = "") String title_like) {
+    //	return dao.searchByTitle(title_like);
+    //}
 }
