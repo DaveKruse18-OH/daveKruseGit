bc3bd7a49cc17969e1a526a260af0dbf96f67eac
Steve Carmichael <steve@techelevator.com>
2020-11-01 11:22:38 -0500

    module 3 review server code

---
+++ module-3/XX-W7-Review-Day1/student/java/te-server/src/main/java/com/techelevator/testudent/controller/StudentController.java
@@ -0,0 +1,122 @@
+package com.techelevator.testudent.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.testudent.dao.StudentDAO;
+import com.techelevator.testudent.domain.Student;
+
+@RestController
+@RequestMapping(path = "/api")
+public class StudentController {
+	
+
+	private StudentDAO studentDAO;
+	
+	
+	public StudentController(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
+	
+/*	
+	/api/students  	(GET)
+	Gets a list of all students
+	Success: 200 - Returns a list of students
+*/	
+	@RequestMapping(path = "/students", method=RequestMethod.GET)
+	public List<Student> retrieveListOfStudents() {
+		return studentDAO.retrieveListOfStudents();
+	}
+	
+	
+	
+/*
+/api/students/:id    (GET)
+	Gets a single student by id
+	Success: Returns a single student if the id is valid
+	Error: returns a 404 if the student id does not exist
+*/	
+	@RequestMapping(path = "/students/{id}", method=RequestMethod.GET)
+	public Student retrieveStudentById(@PathVariable int id) {
+		return studentDAO.retrieveStudentById(id);
+	}
+	
+	
+	
+/*	
+/api/students	(POST)
+	Creates a new student
+	Success: returns a 201 (Created)  when successful
+*/
+	@ResponseStatus(HttpStatus.CREATED)
+	@RequestMapping(path = "/students", method=RequestMethod.POST)
+	public Student addStudent(@Valid @RequestBody Student newStudent) {
+		return studentDAO.addStudent(newStudent);
+	}
+	
+/*	
+/api/students/:id    (PUT)
+	Updates a specific student
+	Success: returns a 200 (OK) when successful
+	Error: returns a 404 if the student id does not exist
+*/	
+	@RequestMapping(path = "/students/{id}", method=RequestMethod.PUT)
+	public Student updateStudent(@Valid @RequestBody Student updateStudent, @PathVariable int id) {
+		return studentDAO.updateStudent(updateStudent, id);
+	}
+	
+	
+/*	
+/api/students/:id    (DELETE)
+	Deletes a specific student
+	Success: returns a 200 (OK) when successful
+	Error: returns a 404 if the id does not exist
+*/	
+	@RequestMapping(path = "/students/{id}", method=RequestMethod.DELETE)
+	public void deleteStudent(@PathVariable int id) {
+		studentDAO.deleteStudent(id);
+		
+	}
+	
+	
+/*	
+/api/students/filter  	(GET)
+
+    Filter students by cohort only
+    Filter students by cohort and section
+             
+	Required Parameters:
+    cohort     =   Gets a list of all students by cohort name
+     
+    Usage example: 	/api/students/filter?cohort=12
+    
+	Optional Parameters:
+	sectionName    =   Gets a list of all students by section
+	Usage example: 	/api/students/filter?cohort=12&section=green
+
+    Optional lastName = Gets a list of students by last name
+    Usage example: 	/api/students/filter?cohort=12&lastName=smith
+
+
+	Success: 200 
+	*/	
+	
+	@RequestMapping(path = "/students/filter", method=RequestMethod.GET)
+	public List<Student> searchStudents(@RequestParam String cohortName,@RequestParam(required=false) String sectionName, @RequestParam(required=false) String lastName) {
+		return studentDAO.searchStudents(cohortName, sectionName, lastName);
+	}
+	
+	
+	
+
+}
