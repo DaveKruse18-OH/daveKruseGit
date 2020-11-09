d8db6d31d2299277f24227d868f436a2fca83f47
dave kruse <dave_kruse@att.net>
2020-11-09 09:53:45 -0500

    initial pull for module #4.

--- module-2/05_Database_Design/student-exercise/ProjectOrganizer.sql
+++ module-2/05_Database_Design/student-exercise/ProjectOrganizer.sql
@@ -1,80 +1,11 @@
-drop table employee_project;
-drop table employee;
-drop table department;
-drop table project;
+insert into transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) values (2, 2, 2, 1, 100.25);
 
+select * from accounts where account_id = 4;
 
-BEGIN TRANSACTION;
+select * from transfers where account_from = 3 or account_to = 3;
 
-CREATE TABLE department
-(
-        departmentNumber serial,
-        departmentName varchar(50) not null,
-        
-        constraint pk_department primary key (departmentNumber)
-        
-);
-
-CREATE TABLE project
-(
-        projectNumber serial,
-        name varchar(100) not null,
-        startDate date,
-        
-        constraint pk_project primary key (projectNumber)
-        
-);
-
-CREATE TABLE employee
-(
-        employeeId serial,
-        jobTitle varchar(25),
-        lastName varchar(64) not null,
-        firstName varchar(64) not null,
-        gender char(1) not null,
-        dateOfBirth date,
-        dateOfHire date,
-        departmentNumber int not null,
-        
-        constraint pk_employee primary key (employeeId),
-        constraint fk_employee_department foreign key (departmentNumber) references department (departmentNumber)
-);
-
-CREATE TABLE employee_project
-(
-        employeeId int not null,
-        projectNumber int not null,
-        
-        constraint pk_employee_project primary key (employeeId, projectNumber),
-        constraint fk_employee_project_employee foreign key (employeeId) references employee (employeeId),
-        constraint fk_employee_project_project foreign key (projectNumber) references project (projectNumber)
-        
-);
-
-insert into project (name, startDate) values ('project1Name', '2020-10-19');
-insert into project (name, startDate) values ('project2Name', '2020-10-19');
-insert into project (name, startDate) values ('project3Name', '2020-10-20');
-insert into project (name, startDate) values ('project4Name', '2020-12-18');
-
-insert into department (departmentName) values ('Accounting');
-insert into department (departmentName) values ('Engineering');
-insert into department (departmentName) values ('Human Resources');
-
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Mechanical Engineer', 'Kruse', 'Marissa', 'F', '1997-10-30', '2019-03-02', 2);
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Software Engineer', 'Kruse', 'David', 'M', '1985-10-18', '2015-07-18', 2);
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Director', 'Smith', 'Sam', 'M', '1998-11-30', '2017-05-03', 3);
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Administrator', 'Yelic', 'Lisa', 'F', '1997-07-30', '2015-12-02', 3);
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Accountant', 'Doe', 'John', 'M', '1995-06-30', '2017-07-15', 1);
-insert into employee (jobTitle, lastName, firstName, gender, dateOfBirth, dateOfHire, departmentNumber) values ('Manager', 'Charles', 'Don', 'M', '1995-03-02', '2018-05-13', 1);
-
-insert into employee_project (employeeId, projectNumber) values (1, 1);
-insert into employee_project (employeeId, projectNumber) values (6, 1);
-insert into employee_project (employeeId, projectNumber) values (2, 2);
-insert into employee_project (employeeId, projectNumber) values (5, 2);
-insert into employee_project (employeeId, projectNumber) values (3, 3);
-insert into employee_project (employeeId, projectNumber) values (4, 3);
-insert into employee_project (employeeId, projectNumber) values (2, 4);
-insert into employee_project (employeeId, projectNumber) values (3, 4);
-
-COMMIT TRANSACTION;
-        
+select * from transfers
+join users on account_from = user_id or account_to = user_id
+join transfer_types on transfers.transfer_type_id = transfer_types.transfer_type_id
+join transfer_statuses on transfers.transfer_status_id = transfer_statuses.transfer_status_id
+where transfer_id = 35;
