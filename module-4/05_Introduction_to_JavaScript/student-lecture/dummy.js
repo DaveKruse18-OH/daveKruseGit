ffbeafb303ac6e74fa404ec1f8394a91c3f0fefc
dave kruse <dave_kruse@att.net>
2020-11-14 15:26:26 -0500

    module #4, exercise #3 work in progress.

---
+++ module-4/05_Introduction_to_JavaScript/student-lecture/dummy.js
@@ -0,0 +1,16 @@
+console.log('this is a log statement');
+
+/*
+multi-line comment.
+*/
+
+// single line comment.
+
+// let allows us to change the value of the variable later (should be our second choice).
+let someNumber = 2;
+console.log(someNumber);
+
+// const allows us to create a constant (should be our first choice).
+const someOtherNumber = 3;
+
+// NEVER USE var!!!!
