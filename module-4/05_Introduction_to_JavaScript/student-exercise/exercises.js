05fa095ceb843809316312893827d31e178d6e5b
dave kruse <dave_kruse@att.net>
2020-11-16 20:01:57 -0500

    module #4, exercise #4 work in progress.

--- module-4/05_Introduction_to_JavaScript/student-exercise/exercises.js
+++ module-4/05_Introduction_to_JavaScript/student-exercise/exercises.js
@@ -7,11 +7,17 @@
 		sumDouble(2, 2) → 8
 
 		function sumDouble(x, y) {
-			// do logic here
+			// put code here.
 			// return result;
-			return x + y;
         }
 */
+function sumDouble(x,y) {
+	if (x == y) {
+		return 2*(x+y);
+	} else {
+		return x+y;
+	}
+}
 
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
@@ -21,6 +27,16 @@
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+function hasTeen(a, b, c) {
+	if ((a > 12) && (a < 20)) {
+		return true;
+	} else if ((b > 12) && (b < 20)) {
+		return true;
+	} else if ((c > 12) && (c < 20)) {
+		return true;
+	}
+	return false;
+}
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
@@ -30,6 +46,12 @@
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+function lastDigit(a, b) {
+	if ((a % 10) == (b % 10)) {
+		return true;
+	}
+	return false;
+}
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
@@ -39,6 +61,15 @@
 		seeColor("xxred") → ""
         seeColor("blueTimes") → "blue"
 */
+function seeColor(a) {
+	if (a.substring(0, 3) == "red") {
+		return "red";
+	}
+	if (a.substring(0,4) == "blue") {
+		return "blue";
+	}
+	return "";
+}
 
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, filters out 
@@ -47,6 +78,16 @@
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+function oddOnly(array) {
+	let returnArray = [];
+	for (let x = 0; x < array.length; x++) {
+		if (!(array[x] % 2) == 0) {
+			returnArray.push(array[x]);
+		}
+	}
+	return returnArray;
+}
+
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -56,6 +97,13 @@
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+function frontAgain(s) {
+	if (s.substring(0,2) == s.substring(s.length-2, s.length)) {
+		return true;
+	}
+	return false;
+}
+
 
 /*
 7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
@@ -68,6 +116,10 @@ or false otherwise.
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
+function cigarParty(a, b) {
+	
+}
+
 
 /*
 8. **fizzBuzz** Because you know you can't live without it, FizzBuzz.
