08bf7e9f393e68ea34701abfd132e649731e934b
dave kruse <dave_kruse@att.net>
2020-11-18 20:20:45 -0500

    module #4 - DOM work in progress.

--- module-4/07_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-4/07_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,17 +1,32 @@
 // add pageTitle
+const pageTitle = "My Shopping List";
 
 // add groceries
+const groceries = ["milk", "cheese", "tomatoes", "celery", "meat", "oranges", "pop", "eggs", "cereal", "noodles"];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  let title = document.getElementById('title').textContent = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+   const groceryList = document.getElementById('groceries');
+   groceries.forEach((item) => {
+       const node = document.createElement('li');
+//       console.log(item);
+       node.innerText = item;
+//       //groceryList.insertAdjacentElement('afterbegin', node);
+//       //groceryList.insertAdjacentElement('beforeend', node);
+//       //groceryList.insertAdjacentElement(node);
+       groceryList.appendChild(node);
+   });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
