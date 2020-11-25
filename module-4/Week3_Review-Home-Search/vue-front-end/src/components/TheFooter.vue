14ac491cc0ab92e682ec507000c8dff0fe0c3fe3
Steve Carmichael <steve@techelevator.com>
2020-11-25 13:50:22 -0500

    added class lecture code

---
+++ module-4/Week3_Review-Home-Search/vue-front-end/src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>&#169; {{copyText}} </p>
+  </div>    
+</template>
+
+<script>
+export default {
+    name: 'the-footer',
+    data() {
+        return {
+           copyText: 'Java Green Enterprises LLC. All rights reserved.'
+        };
+    }
+
+}
+</script>
+
+<style>
+
+</style>
