1753ed859a981a2826e6a440d1697071f96d36da
dave kruse <dave_kruse@att.net>
2020-12-01 21:03:32 -0500

    module #4 / Exercise #12 work in progress

--- module-4/12_Vue_Routing/student-exercise/src/router/index.js
+++ module-4/12_Vue_Routing/student-exercise/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/views/Home';
 
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
 ];
 
 const router = new VueRouter({
---
+++ module-4/12_Vue_Routing/student-exercise/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <best-seller-list/>
+        <new-releases-list/>
+  </div>
+</template>
+
+<script>
+import BestSellerList from '@/components/BestSellerList';
+import NewReleasesList from '@/components/NewReleasesList';
+
+export default {
+    name: 'home',
+    components: {
+        BestSellerList,
+        NewReleasesList
+    },
+    created() {
+
+    }
+};
+</script>
+
+<style>
+
+</style>
