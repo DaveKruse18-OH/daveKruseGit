c739f96b007d9c063d9735ba08c3846b2178c806
dave kruse <dave_kruse@att.net>
2020-12-02 20:59:38 -0500

    module #4 / Exercise #12 work in progress

--- module-4/12_Vue_Routing/student-exercise/src/App.vue
+++ module-4/12_Vue_Routing/student-exercise/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <nav>
+      <a href='#' v-on:click.prevent="goHome">Home</a>&nbsp;<a href='#' v-on:click.prevent="goMyBooks">Go Home</a>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -7,8 +10,17 @@
 <script>
 
 export default {
-  name: 'app'
-}
+  name: 'app',
+  methods: {
+    goHome() {
+      this.$router.push({name: 'home'});
+    },
+    goMyBooks() {
+      this.$router.push({name: 'my-books'})
+    }
+  }
+};
+
 </script>
 
 <style>
---
+++ module-4/12_Vue_Routing/student-exercise/src/views/MyBooks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <reading-list/>
+    </div>
+</template>
+
+<script>
+import ReadingList from '@/components/ReadingList';
+
+export default {
+   name: 'my-books',
+    components: {
+        ReadingList
+    },
+    created() {
+
+    }
+};
+</script>
+
+<style>
+
+</style>
