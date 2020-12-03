7798d682f019afe7114b423fb1c391a369c9a150
dave kruse <dave_kruse@att.net>
2020-12-02 21:21:59 -0500

    module #4 / Exercise #12 work in progress

--- module-4/12_Vue_Routing/student-exercise/src/router/index.js
+++ module-4/12_Vue_Routing/student-exercise/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import MyBooks from '../views/MyBooks.vue';
+import NewBook from '../views/NewBook.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/myBooks',
     name: 'my-books',
     component: MyBooks
+  },
+  {
+    path: '/addBook',
+    name: 'add-book',
+    component: NewBook
   }
 ];
 
---
+++ module-4/12_Vue_Routing/student-exercise/src/views/NewBook.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <new-book-form/>
+    </div>
+</template>
+
+<script>
+import NewBookForm from '@/components/NewBookForm';
+
+export default {
+  name: 'new-book',
+    components: {
+        NewBookForm
+    },
+    created() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
